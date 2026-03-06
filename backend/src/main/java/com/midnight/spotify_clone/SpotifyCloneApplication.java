525dd415dce420bdc25d68df5e0b83305457dff2
midnightph <pedrorodacinski26@gmail.com>
2026-03-06 20:10:34 -0300

    First commit

---
+++ backend/src/main/java/com/midnight/spotify_clone/SpotifyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.midnight.spotify_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneApplication.class, args);
+	}
+
+}
