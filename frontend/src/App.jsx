a5a58a7789cd1eec569a64d1c2596c87bffc9b76
midnightph <pedrorodacinski26@gmail.com>
2026-03-08 07:35:06 -0300

    feat: refactor App component structure; simplify routing and user ID handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/login/page'
 import Home from './pages/home/page'
-import PageTransition from './pages/component/PageTransition'
+import './App.css'
 import { useEffect } from 'react'
 
-function AnimatedRoutes() {
-    const location = useLocation()
-
-    return (
-        <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<PageTransition><Home /></PageTransition>} />
-                <Route path="/login" element={<PageTransition><Login /></PageTransition>} />
-            </Routes>
-        </AnimatePresence>
-    )
-}
-
 function App() {
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search)
-        const userId = params.get("userId")
-        if (userId) {
-            localStorage.setItem("userId", userId)
-            window.history.replaceState({}, "", "/")
-        }
-    }, [])
 
-    return (
-        <Router>
-            <AnimatedRoutes />
-        </Router>
-    )
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const userId = params.get("userId")
+
+    if (userId) {
+      localStorage.setItem("userId", userId)
+      window.history.replaceState({}, "", "/")
+    }
+  }, [])
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
